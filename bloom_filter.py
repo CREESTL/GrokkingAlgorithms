3d27d4feac0d65c11312036d651e1fd523671a02
CREESTL <kroymw3@yandex.ru>
2020-12-10 17:38:37 +0300

    bloom filter doesnt wok on linux?

---
+++ bloom_filter.py
@@ -0,0 +1,14 @@
+'''
+
+Полное объяснение можно найти здесь:
+https://www.geeksforgeeks.org/bloom-filters-introduction-and-python-implementation/
+
+Теории очень много, поэтому писать здесь ее не вижу смысла
+
+'''
+
+import math
+# murmurhash
+import mmh3
+from bitarray import bitarray
+
